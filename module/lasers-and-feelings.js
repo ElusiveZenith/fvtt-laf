d28b1be9f98e463276008e1f1ffee041688fcd03
Paul C <pacosgrove1@googlemail.com>
2023-11-29 15:18:05 +0000

    simplify chat output for rolls, make dice and result clearer

--- module/lasers-and-feelings.js
+++ module/lasers-and-feelings.js
@@ -3,18 +3,18 @@
  * @param {integer} num - An integer value found on the Lasers & Feelings character sheet in the number input
  * @param {RollTypes} roleType - Lasers or Feelings roll
  * @param {string} characterName - Name of the character rolling
-*/
+ */
 async function makeRole(diceString, num, roleType, characterName) {
-  let roll = await new Roll(diceString).roll();
-  let chatTemplate = evaluateRolls(roll, num, roleType);
-  roll.toMessage({
-    speaker: { alias: characterName },
-  });
+  let roll = await new Roll(diceString).roll()
+  let chatTemplate = evaluateRolls(roll, num, roleType)
+
   ChatMessage.create({
     content: chatTemplate,
     roll: roll,
+    sound: CONFIG.sounds.dice,
+    type: CONST.CHAT_MESSAGE_TYPES.ROLL,
     speaker: { alias: characterName },
-  });
+  })
 }
 
 /**
@@ -22,124 +22,141 @@ async function makeRole(diceString, num, roleType, characterName) {
  * @param {integer} num - An integer value found on the Lasers & Feelings character sheet in the number input
  * @param {RollTypes} roleType - an integer value of 1 or 0; 1 indicates a Lasers roll, 0 indicates a Feelings roll
  * @returns {string} resultContent - the final string result of number of successes and any Laser-Feelings to be returned
-*/
+ */
 function evaluateRolls(r, num, roleType) {
-  var array = r.terms[0].results;
-  var successCount = 0;
-  var laserFeelingsCount = 0;
-  var resultContent = ``;
+  var successCount = 0
+  var laserFeelingsCount = 0
 
-  array.forEach((element) => {
+  const results = r.dice.flatMap((d) =>
+    d.results.map((result) => result.result),
+  )
+
+  const label =
+    roleType == RollTypes.Lasers
+      ? game.i18n.localize('SIMPLE.LasersRoll')
+      : game.i18n.localize('SIMPLE.FeelingsRoll')
+
+  var resultContent = `<p>${label}</p>`
+  let resultClass = ''
+
+  resultContent += `
+    <div class="dice-tooltip">
+      <ol class="dice-rolls">`
+
+  for (const result of results) {
+    resultClass = ''
     if (
-      (element.result <= num && roleType == RollTypes.Lasers) ||
-      (element.result >= num && roleType == RollTypes.Feelings)
+      (result <= num && roleType == RollTypes.Lasers) ||
+      (result >= num && roleType == RollTypes.Feelings)
     ) {
-      successCount = successCount + 1;
-      if (element.result == num) {
-        laserFeelingsCount = laserFeelingsCount + 1;
+      resultClass = 'success'
+      successCount = successCount + 1
+      if (result == num) {
+        laserFeelingsCount = laserFeelingsCount + 1
       }
     }
-  });
 
-  if (laserFeelingsCount > 0) {
-    resultContent = `
-      <p class="roll success">${game.i18n.localize(
-        "SIMPLE.Successes"
-      )}: ${successCount}</p> 
-      <p class="roll laser-feelings">Laser Feelings: ${laserFeelingsCount}</p>
-    `;
-  } else {
-    resultContent = `
-      <p class="roll success">${game.i18n.localize(
-        "SIMPLE.Successes"
-      )}: ${successCount}</p> 
-    `;
+    resultContent += `<li class="roll die d6 ${resultClass}">${result}</li>`
   }
+  resultContent += `
+      </ol>
+    </div>`
 
-  return resultContent;
+  resultContent += `
+  <p class="roll success">${game.i18n.localize(
+    'SIMPLE.Successes',
+  )}: ${successCount}</p> 
+  `
+
+  if (laserFeelingsCount > 0)
+    resultContent += `
+      <p class="roll laser-feelings">Laser Feelings: ${laserFeelingsCount}</p>
+    `
+
+  return resultContent
 }
 
 /**
  * @param {String} characterName - The name of the character passed in as a string from actor-sheet.js
-*/
+ */
 function lasersRoll(characterName) {
-  let dialogTemplate = game.i18n.localize("SIMPLE.LasersRoll");
-  let thisActor = game.actors.getName(characterName);
-  let num = thisActor.data.data.theOnlyStat;
+  let dialogTemplate = game.i18n.localize('SIMPLE.LasersRoll')
+  let thisActor = game.actors.getName(characterName)
+  let num = thisActor.data.data.theOnlyStat
   new Dialog({
-    title: game.i18n.localize("SIMPLE.LasersRoll"),
+    title: game.i18n.localize('SIMPLE.LasersRoll'),
     content: dialogTemplate,
     buttons: {
       normal: {
         label: game.i18n.localize(Preparedness.Normal),
         callback: () => {
-          makeRole("1d6", num, RollTypes.Lasers, characterName);
+          makeRole('1d6', num, RollTypes.Lasers, characterName)
         },
       },
       prepared: {
         label: game.i18n.localize(Preparedness.Prepared),
         callback: () => {
-          makeRole("2d6", num, RollTypes.Lasers, characterName);
+          makeRole('2d6', num, RollTypes.Lasers, characterName)
         },
       },
       expert: {
         label: game.i18n.localize(Preparedness.Expert),
         callback: () => {
-          makeRole("3d6", num, RollTypes.Lasers, characterName);
+          makeRole('3d6', num, RollTypes.Lasers, characterName)
         },
       },
       close: {
-        label: "Close",
+        label: 'Close',
       },
     },
-  }).render(true);
+  }).render(true)
 }
 
 /**
  * @param {String} characterName - The name of the character passed in as a string from actor-sheet.js
-*/
+ */
 function feelingsRoll(characterName) {
-  let dialogTemplate = game.i18n.localize("SIMPLE.FeelingsRoll");
-  let thisActor = game.actors.getName(characterName);
-  let num = thisActor.data.data.theOnlyStat;
+  let dialogTemplate = game.i18n.localize('SIMPLE.FeelingsRoll')
+  let thisActor = game.actors.getName(characterName)
+  let num = thisActor.data.data.theOnlyStat
   new Dialog({
-    title: game.i18n.localize("SIMPLE.FeelingsRoll"),
+    title: game.i18n.localize('SIMPLE.FeelingsRoll'),
     content: dialogTemplate,
     buttons: {
       normal: {
         label: game.i18n.localize(Preparedness.Normal),
         callback: () => {
-          makeRole("1d6", num, RollTypes.Feelings, characterName);
+          makeRole('1d6', num, RollTypes.Feelings, characterName)
         },
       },
       prepared: {
         label: game.i18n.localize(Preparedness.Prepared),
         callback: () => {
-          makeRole("2d6", num, RollTypes.Feelings, characterName);
+          makeRole('2d6', num, RollTypes.Feelings, characterName)
         },
       },
       expert: {
         label: game.i18n.localize(Preparedness.Expert),
         callback: () => {
-          makeRole("3d6", num, RollTypes.Feelings, characterName);
+          makeRole('3d6', num, RollTypes.Feelings, characterName)
         },
       },
       close: {
-        label: "Close",
+        label: 'Close',
       },
     },
-  }).render(true);
+  }).render(true)
 }
 
 const RollTypes = {
   Lasers: 1,
-  Feelings: 0
-};
+  Feelings: 0,
+}
 
 const Preparedness = {
-  Normal: "SIMPLE.Normal",
-  Prepared: "SIMPLE.Prepared",
-  Expert: "SIMPLE.Expert",
-};
+  Normal: 'SIMPLE.Normal',
+  Prepared: 'SIMPLE.Prepared',
+  Expert: 'SIMPLE.Expert',
+}
 
-export { lasersRoll, feelingsRoll };
+export { lasersRoll, feelingsRoll }
